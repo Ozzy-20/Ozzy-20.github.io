8b72df7310df8497474e7c8aedfb7227fd143d51
Oswaldo Olson <ooswaldo2022@gmail.com>
2023-04-23 17:21:51 -0700

    updating thegoodgoodla changes

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+// This code makes the "pastriesContent" visible and hides the "drinksContent"
+// when the "pastriesButton" is clicked.
+
 document.getElementById("pastriesButton").addEventListener("click", function() {
   var pastriesContent = document.getElementById("pastriesContent");
-  pastriesContent.style.display = "block";
+  pastriesContent.style.display = "block"; // Show pastriesContent
   var drinksContent = document.getElementById("drinksContent");
-  drinksContent.style.display = "none";
+  drinksContent.style.display = "none"; // Hide drinksContent
 });
+
+
+// This code makes the "drinksContent" visible and hides the "pastriesContent"
+// when the "drinksButton" is clicked.
 
 document.getElementById("drinksButton").addEventListener("click", function() {
   var drinksContent = document.getElementById("drinksContent");
-  drinksContent.style.display = "block";
+  drinksContent.style.display = "block"; // Show drinksContent
   var pastriesContent = document.getElementById("pastriesContent");
-  pastriesContent.style.display = "none";
+  pastriesContent.style.display = "none"; // Hide pastriesContent
 });
